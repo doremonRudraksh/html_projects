0ada9e77d78ddcfb7a8194667ebf9b4e580bf04a
doremonRudraksh <rudraksh905.sharma@gmail.com>
2024-03-29 16:19:20 +0530

    first commit

---
+++ practixe activity/main.js
@@ -0,0 +1,23 @@
+function textBlueH1(){
+    document.getElementById("h1").style.color = "blue";
+    console.log("color blue of h1");
+}
+
+function textGreenH1(){
+    document.getElementById("h1").style.color = "green";
+    console.log("color green of h1");
+}
+
+function textYellowH1(){
+    document.getElementById("h1").style.color = "Yellow";
+    console.log("color yellow of h1");
+}
+
+function textRedH1(){
+    document.getElementById("h1").style.color = "red";
+    console.log("color red of h1");
+}
+
+
+
+
